fb4bc3f91b680e2630a94f354dc1ecb922d9d998
Shkalin Vyacheslav <Shkalin2020@yandex.ru>
2022-12-19 11:28:14 +0300

    add model: Info

---
+++ src/main/java/ru/kpfu/itis/shkalin/simplifytorrent/model/Info.java
@@ -0,0 +1,9 @@
+package ru.kpfu.itis.shkalin.simplifytorrent.model;
+
+public class Info {
+    private String hashMD5;
+    private Integer pieceLength;
+    private String piecesHashesSHA1;
+    private Long fileLength;
+    private String title;
+}
