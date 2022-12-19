a152652d0f85a107ccaa228f68026c18fc6f75b2
Shkalin Vyacheslav <Shkalin2020@yandex.ru>
2022-12-19 11:29:32 +0300

    add service: AssemblingPiecesService

---
+++ src/main/java/ru/kpfu/itis/shkalin/simplifytorrent/service/AssemblingPiecesService.java
@@ -0,0 +1,11 @@
+package ru.kpfu.itis.shkalin.simplifytorrent.service;
+
+import ru.kpfu.itis.shkalin.simplifytorrent.model.Piece;
+
+public class AssemblingPiecesService {
+
+    public void put (Piece piece) {
+
+    }
+
+}
