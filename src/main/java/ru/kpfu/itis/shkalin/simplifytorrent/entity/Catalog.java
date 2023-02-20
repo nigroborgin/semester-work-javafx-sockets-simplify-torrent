2031cc4659d93a0b4c9b1eb159a0115ac80f2eee
Shkalin Vyacheslav <Shkalin2020@yandex.ru>
2023-02-20 12:06:23 +0300

    add Catalog entity

---
+++ src/main/java/ru/kpfu/itis/shkalin/simplifytorrent/entity/Catalog.java
@@ -0,0 +1,32 @@
+package ru.kpfu.itis.shkalin.simplifytorrent.entity;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Catalog extends HashMap<String, Info> implements Serializable {
+
+    public Catalog(Map<String, Info> map) {
+        this.putAll(map);
+    }
+
+    public Catalog() {
+
+    }
+
+//    @Override
+//    public String toString() {
+//        StringBuilder sb = new StringBuilder();
+//
+//        Object[] keys = super.keySet().toArray();
+//        sb.append("Catalog{\n");
+//
+//        for (int i = 0; i < keys.length; i++) {
+//            sb.append("\n");
+//            sb.append("key: " + keys[i].toString() + "\n");
+//            sb.append("value: " + super.get(keys[i]).toString() + "\n");
+//        }
+//        sb.append("}");
+//        return sb.toString();
+//    }
+}
