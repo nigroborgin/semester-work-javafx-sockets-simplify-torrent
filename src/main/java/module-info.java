bb3626df849ea89154c080fd9887baa0ea513d37
Shkalin Vyacheslav <Shkalin2020@yandex.ru>
2023-02-20 12:22:00 +0300

    small edit module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,10 +11,19 @@ module ru.kpfu.itis.shkalin.simplifytorrent {
     opens ru.kpfu.itis.shkalin.simplifytorrent.dto;
     opens ru.kpfu.itis.shkalin.simplifytorrent.entity;
     opens ru.kpfu.itis.shkalin.simplifytorrent.service;
+    opens ru.kpfu.itis.shkalin.simplifytorrent.protocol;
+    opens ru.kpfu.itis.shkalin.simplifytorrent.protocol.message;
+    opens ru.kpfu.itis.shkalin.simplifytorrent.protocol.message.fields;
+    opens ru.kpfu.itis.shkalin.simplifytorrent.protocol.message.exception;
 
     exports ru.kpfu.itis.shkalin.simplifytorrent;
     exports ru.kpfu.itis.shkalin.simplifytorrent.controller;
     exports ru.kpfu.itis.shkalin.simplifytorrent.dto;
     exports ru.kpfu.itis.shkalin.simplifytorrent.entity;
     exports ru.kpfu.itis.shkalin.simplifytorrent.service;
+    exports ru.kpfu.itis.shkalin.simplifytorrent.protocol;
+    exports ru.kpfu.itis.shkalin.simplifytorrent.protocol.message;
+    exports ru.kpfu.itis.shkalin.simplifytorrent.protocol.message.fields;
+    exports ru.kpfu.itis.shkalin.simplifytorrent.protocol.message.exception;
+
 }
